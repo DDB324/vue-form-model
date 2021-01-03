256ab774926f909189ac8f04a967f43f5096d555
ddb324 <jiangdaoran@163.com>
2021-01-03 19:46:31 +0800

    textarea

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <div id="app">
-    <input placeholder="edit me" v-model="message">
+    <textarea placeholder="edit me" v-model="message" />
     <p>Message is: {{message}}</p>
     <p><button @click="message = 'ddb'">set message to ddb</button></p>
   </div>
